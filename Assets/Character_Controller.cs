99b10d83a11fc9e82f6a90d50bace8f8c977d954
Impalord <13741@student.vgu.edu.vn>
2020-03-30 21:36:15 +0700

    only learning purpose

---
+++ Assets/Character_Controller.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Character_Controller : MonoBehaviour
+{
+    private Animator ani; // animator object
+
+
+    // Value setting
+    private float directionalDampTime = 0.25f;
+    private float speed;
+    private float horizontal;
+    private float vertical;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ani = GetComponent<Animator>();
+        if(ani.layerCount >= 2)
+        {
+            ani.SetLayerWeight(1, 1);
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        if (ani)
+        {
+            horizontal = Input.GetAxis("Horizontal");  // Get Horizontal from Input
+            vertical = Input.GetAxis("Vertical");    // Get Vertical from Input
+            speed = new Vector2(horizontal, vertical).sqrMagnitude; // Set speed
+
+            // Setting up variable connected to Animator in Unity
+            ani.SetFloat("Speed", speed);
+            ani.SetFloat("Direction", horizontal, directionalDampTime, Time.deltaTime);
+
+
+        }
+
+
+
+        
+    }
+}
---
+++ Assets/ThirdPersonCam.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCam : MonoBehaviour
+{
+    // Variable Distance of Camera from Player
+    private float distanceAway = 5;
+    private float distanceUp = 2;
+    [SerializeField]
+    private float smooth;
+    private Transform follow;
+    private Vector3 targetposition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        follow = GetComponent<Transform>();
+        follow = GameObject.FindWithTag("Player").transform;
+
+    }
+
+    void LateUpdate()
+    {
+        // Make a offset for camera 
+        Vector3 Charoffset = follow.position + new Vector3(0f, distanceUp, 0f);
+
+        // Calculate the postion for camera position 
+        targetposition = follow.position + Vector3.up * distanceUp - follow.forward * distanceAway;
+        Debug.DrawRay(follow.position, Vector3.up * distanceUp, Color.red);
+        Debug.DrawRay(follow.position, -1f * follow.forward * distanceAway, Color.blue);
+        Debug.DrawLine(follow.position, targetposition, Color.green);
+
+        // making Smooth cam
+
+        transform.position = Vector3.Lerp(transform.position, targetposition, Time.deltaTime * smooth);
+
+        // Lookat Right WAY
+        transform.LookAt(follow);
+        cameraCollision(Charoffset,  ref targetposition);
+    }
+    private void cameraCollision(Vector3 from, ref Vector3 to)
+    {
+        Debug.DrawLine(from, to, Color.cyan);
+
+        RaycastHit wallHit = new RaycastHit();
+        if( Physics.Linecast(from, to, out wallHit))
+        {
+            Debug.DrawRay(wallHit.point, Vector3.left, Color.red);
+            to = new Vector3(wallHit.point.x, to.y, wallHit.point.z);
+        }
+    }
+}
